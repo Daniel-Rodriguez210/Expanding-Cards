f1ca2d4f5f614822b80f698988f1a310bc4222ef
Daniel Rodriguez <drod18@hotmail.com>
2022-03-30 08:46:37 -0500

    adding notes to javascript code

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel') //Selecting all cards with class of panel
 
+//This code removes the active class from all the cards when clicking on any card with calling removeActiveClasses function. Also, it makes whichever card is clicked active.
 panels.forEach((panel) =>{
     panel.addEventListener('click',() =>{
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
-
+//This function removes active class from all cards. 
 function removeActiveClasses() {
     panels.forEach((panel) =>{
         panel.classList.remove('active')
